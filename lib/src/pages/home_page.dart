5d7f54da770fce71d8b5c7f9d9f3527cbc59d695
Afek-Sakaju <100536372+Afek-Sakaju@users.noreply.github.com>
2023-12-21 09:16:31 +0200

    Created the Home page initial design and content

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -5,10 +5,43 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Hello World!'),
-      ),
+    return Scaffold(
+      body: Container(
+          decoration: const BoxDecoration(
+              gradient: LinearGradient(
+            colors: [
+              Color.fromARGB(255, 64, 112, 175),
+              Color.fromARGB(255, 84, 126, 180),
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          )),
+          child: Center(
+            child: Column(mainAxisSize: MainAxisSize.min, children: [
+              Image.asset('assets/images/quiz-logo.png', width: 250),
+              const SizedBox(
+                height: 60,
+              ),
+              const Text(
+                'Learn Flutter the fun way!',
+                style: TextStyle(
+                  color: Color.fromARGB(255, 236, 236, 236),
+                  fontSize: 18,
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              TextButton(
+                  onPressed: () {},
+                  child: const Text(
+                    'Start Quiz',
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ))
+            ]),
+          )),
     );
   }
 }
