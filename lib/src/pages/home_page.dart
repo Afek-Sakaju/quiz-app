86e4cc3b2f634091a78743944419cf199f30f2b2
Afek-Sakaju <100536372+Afek-Sakaju@users.noreply.github.com>
2023-12-21 16:47:13 +0200

    changed the structure of the app so that the home page will contain the center and its content, and the main file will have the scaffold and the container, to reuse the same scaffold style for multi pages

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -16,38 +16,7 @@ class HomePage extends StatelessWidget {
             begin: Alignment.topLeft,
             end: Alignment.bottomRight,
           )),
-          child: Center(
-            child: Column(mainAxisSize: MainAxisSize.min, children: [
-              Image.asset(
-                'assets/images/quiz-logo.png',
-                width: 250,
-                color: Color.fromARGB(157, 255, 255, 255),
-              ),
-              const SizedBox(
-                height: 60,
-              ),
-              const Text(
-                'Learn Flutter the fun way!',
-                style: TextStyle(
-                  color: Color.fromARGB(255, 236, 236, 236),
-                  fontSize: 18,
-                ),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              OutlinedButton.icon(
-                  icon: const Icon(Icons.play_circle_filled_rounded),
-                  onPressed: () {},
-                  style: OutlinedButton.styleFrom(
-                    foregroundColor: Colors.white,
-                    backgroundColor: Colors.blueAccent,
-                  ),
-                  label: const Text(
-                    'Start Quiz',
-                  ))
-            ]),
-          )),
+          child: ),
     );
   }
 }
